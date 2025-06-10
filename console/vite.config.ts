cb6d338dd6f865ecee7c9a32881fccf2b676ab19
Ryan Wang <i@ryanc.cc>
2025-06-10 17:58:23 +0800

    chore: update infrastructure
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -1,44 +1,14 @@
 import { fileURLToPath, URL } from "url";
 
+import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
+import Vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [Vue(), HaloUIPluginBundlerKit()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    outDir: fileURLToPath(
-      new URL("../src/main/resources/console", import.meta.url)
-    ),
-    emptyOutDir: true,
-    lib: {
-      entry: "src/index.ts",
-      name: "PluginStackEdit",
-      formats: ["iife"],
-      fileName: () => "main.js",
-    },
-    rollupOptions: {
-      external: [
-        "vue",
-        "@halo-dev/console-shared",
-        "@halo-dev/components",
-        "vue-router",
-      ],
-      output: {
-        globals: {
-          vue: "Vue",
-          "vue-router": "VueRouter",
-          "@halo-dev/components": "HaloComponents",
-          "@halo-dev/console-shared": "HaloConsoleShared",
-        },
-        extend: true,
-      },
     },
   },
 });
